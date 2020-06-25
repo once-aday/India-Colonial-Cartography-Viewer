1f45037b829ad6c2341bd6569676c10ec1d10cb4
once-aday <onceaday@gmail.com>
2020-06-24 18:47:44 -0700

    additional cards and images

--- card_data.js
+++ card_data.js
@@ -4,13 +4,13 @@ var cardData = {
     
     '1':[['images/carrey_crop.png'],"This 1800 Map of India was <a href='https://collections.leventhalmap.org/search/commonwealth:3f462z73w' target='_blank'>published in Philadelphia, PA</a> by Matthew Carey as part of a “General Atlas” during a century when cartography was beginning to establish itself as an important tool for political and societal influence in America.", 0, ["layer_1", 0, [-550,525]], "none"],
     
-    '2':[['images/stat_atlas_cover.png'],"Data was being exhibited and displayed in new ways. Susan Schulten writes '<i>forms</i> of communication were playing an <a href='https://press.uchicago.edu/ucp/books/book/chicago/M/bo13141427.html' target='_blank'>increasingly important role in decision making.'</a> There began an 'enduring view of maps as scientific documents, self-evident forms of communication that were independent of interpretation'", 0, ["layer_1", 0, [-550,525]], "none"],
+    '2':[['images/stat_atlas_cover.png'],"The 1800s saw data exhibited and displayed in new and innovative ways. Susan Schulten writes '<i>forms</i> of communication were playing an <a href='https://press.uchicago.edu/ucp/books/book/chicago/M/bo13141427.html' target='_blank'>increasingly important role in decision making.'</a> There began an 'enduring view of maps as scientific documents, self-evident forms of communication that were independent of interpretation.'", 0, ["layer_1", 0, [-550,525]], "none"],
     
     '3':[['images/globe_hand_crop.png'],"We will briefly explore the potential for British and European bias within this map of India and consider the idea that as geographer Jeremy Crampton states, <a href='https://www.nhbs.com/introducing-human-geographies-book' target='_blank'>“Maps frame the narrative.. [and] All maps are political interventions.”</a>", 0, ["layer_1", 0, [-550,525]], "none"],
 
     '4': [['images/globe_hand_crop.png'],"“An Accurate MAP of HINDOSTAN OR INDIA, from the best AUTHORITIES” was originally released as part of a 1796 textbook “A New Geographical, Historical, and Commercial Grammar;” by historian William Guthrie of London. Carey reproduced the maps as part of his “Carey's American edition of Guthrie's Geography Improved.”", 0, ["layer_1", 1, [-710,755]], "none"],
     
-    '5': [['images/globe_hand_crop.png', 'images/guthrie_book_cover.png'],"So, who are “the best AUTHORITIES”? Based on the maps recognition that this is an American Edition of Guthrie’s Geography, published in London, we can interpret that the 'authorities' are of British origin.", 1, 0, "none"],
+    '5': [['images/globe_hand_crop.png', 'images/guthrie_book_cover.png'],"So, who are “the best AUTHORITIES”? Based on the map's recognition that this is an American Edition of Guthrie’s Geography, published in London, we can interpret that the 'authorities' are of British origin.", 1, 0, "none"],
     
     '6': [['images/EIC_coat_of_arms.png', 'images/company_rule_govt_house.jpg'],"When Guthrie's map 'Hindostan or India' was produced much of India was in a period of “Company Rule” by the British East India Company. At the time Britain was not interested in formally ruling over India and instead wielded control by using the EIC to establish its own government, backed by the British military.", 1, 0, "none"],
     
@@ -26,9 +26,15 @@ var cardData = {
     
     '12': [['images/little_known_to2.png'],"Again we see an area little known to Europeans. There are many admissions of ignorance within the map itself. The borders are nearly all dotted lines, typically signifiying an estimated boundary, perhaps even a <i>lack</i> of authority.", 0, ["layer_1", 2, [-310,370]], "none"],
     
-     '13': [['images/globe_hand_crop.png'],"There are many aspects of this map that reveal potential bias and a European point of view. Directed by British political institutions, the East India Company colonized parts of India and influenced the availability of information to geographers abroad. ", 0, ["layer_1", 0, [-475,615]], "none"],
+     '13': [['images/globe_hand_full.jpg'],"Directed by British political institutions, the East India Company colonized parts of India, potentially influencing the availability of information to geographers abroad.", 0, ["layer_1", 0, [-475,615]], "none"],
     
-    '14': [['images/globe_hand_crop.png'],"The 'best authorities' that the map proclaims to have been involved are perhaps only the best available, but not the best possible.", 0, ["layer_1", 1, [-710,755]], "none"],
+    '14': [['images/mughal_court.jpg']," The Mughal empire of India, ruling since the 1520s, preceded the EIC's occupation. The indegenous people of that culture and that time are an important authority to call upon when mapping because the subject is their land and their history. Land borders, regions of cultural significance, even complete cities may be missing or distorted on this map.", 0, ["layer_1", 0, [-475,475]], "none"],
+    
+    '15': [['images/bridge.png'],"With the origins and political history of the map maker's in mind we can now interpret the map <a href='http://dx.doi.org/10.1080/03736245.1999.9713657' target='_blank'>as C. Kelso deems</a>, a '<i>subjective, socially-produced image which reflects, not reality, but the social, political and cultural context from which it emerged.</i>' ", 0, ["layer_1", 0, [-475,475]], "none"],
+    
+    '16': [['images/globe_hand_full.jpg'],"Kelso Continues.. '<i>As such, the map is viewed as a resource of knowledge which can be used to reflect (both consciously and subconscously) the ideology of the powerful.</i>'", 0, ["layer_1", 0, [-475,475]], "none"],
+    
+    '17': [['images/ganges_canal.jpg'],"With no reference to indegenous data sources, and printed admissions of european knowledge gaps, the prospect of accuracy becomes dubious. The 'best authorities' that the map proclaims to have been involved were perhaps the best made available, but not the best possible.", 0, ["layer_1", 1, [-710,755]], "none"],
      
 }
 
